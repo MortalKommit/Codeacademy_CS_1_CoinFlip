289cf054c70f914c045802dc6849cf938a3215e2
MortalKommit <kedzko@gmail.com>
2020-07-15 12:04:56 +0530

    Added requirements.txt as pipenv alternative updated error handling to handle response object not being created in case of error Fixed proper status code checking

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 import requests
 from requests.adapters import HTTPAdapter
-from requests.packages.urllib3 import Retry
+from urllib3.util import Retry
 import random
 import logging
 
@@ -22,7 +22,7 @@ class TimeoutHTTPAdapter(HTTPAdapter):
         return super().send(request, **kwargs)
 
 
-#logging object configuration (root logger)
+#logging object configuration (root logger) - writes errors to app.log
 logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
 
 #modify requests retry policy
@@ -30,6 +30,8 @@ retries = Retry(total=3, backoff_factor=1, status_forcelist=[429, 500, 502, 503,
 http = requests.Session()
 http.mount("https://", TimeoutHTTPAdapter(max_retries=retries))
 
+''' fetches coin toss by [0,1] random result based on random.org API 
+   unless network error/connection unavailable '''
 def toss_coin():
     payload = {'num': 1, 'min':0, 'max': 1, 'col':1, 'base':10, 'format':'plain', 'rnd':'new'}
     response = None
@@ -39,11 +41,14 @@ def toss_coin():
     except requests.exceptions.RequestException as e:
         logging.error(f'Request Exception (Connection Error)\nDetails:{e.__context__}')
         errored = True
-    if getattr(response, 'status_code', None) == '200' and not errored:
-        return int(response.text.split('\n')[0])
+    if not errored:
+        if getattr(response, 'status_code', None) == 200:
+            return int(response.text.split('\n')[0])
     
     return random.randint(0,1)
-    
+
+''' fetches dice roll from random.org API with a modified payload
+   unless network error/connection unavailable '''   
 def roll_die():
     payload = {'num': 1, 'min':0, 'max': 5, 'col':1, 'base':10, 'format':'plain', 'rnd':'new'}
     response = None
@@ -53,9 +58,11 @@ def roll_die():
     except requests.exceptions.RequestException as e:
         logging.error(f'Request Exception (Connection Error)\nDetails:{e.__context__}')
         errored = True
-    if getattr(response, 'status_code', None) == '200' and not errored:
-        return int(response.text.split('\n')[0])
+    if not errored:
+        if getattr(response, 'status_code', None) == 200:
+            return int(response.text.split('\n')[0])
     return random.randint(0,5)
+
 def play_coin_game():
     guess = input('Choose ([H]eads/[T]ails):')
     while guess.upper() not in ('H', 'T'):
@@ -75,10 +82,13 @@ def play_dice_game():
         print('Error! Must enter a number!')
     print(f'Your guess:{guess}')
     return int(guess)
+
+#Function that calls other game functions
 def play_guess_game():
     successful_guesses = 0
     total_guesses = 0
     replay = True
+    #Stores result out comes (Mostly for the cointoss game)
     possible_outcomes = {'cointoss':('Heads', 'Tails'), 'diceroll':tuple(i for i in range(1,7))}
 
     while replay:
